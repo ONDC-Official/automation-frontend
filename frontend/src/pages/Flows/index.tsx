2129e8f66349aa2e048e105dfece8b8a1e953da8
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2024-12-13 14:00:56 +0530

    updated env variale

--- frontend/src/pages/Flows/index.tsx
+++ frontend/src/pages/Flows/index.tsx
@@ -29,7 +29,7 @@ const Flows = () => {
 
   const getFlows = async () => {
     const response = await axios.get(
-      `${import.meta.env.VITE_SOME_KEY}/flow`,
+      `${import.meta.env.VITE_BACKEND_URL}/flow`,
       {}
     );
 
@@ -49,7 +49,7 @@ const Flows = () => {
 
     var config = {
       method: "put",
-      url: `${import.meta.env.VITE_SOME_KEY}/sessions?subscriber_url=${
+      url: `${import.meta.env.VITE_BACKEND_URL}/sessions?subscriber_url=${
         session.subscriberUrl
       }`,
       headers: {
