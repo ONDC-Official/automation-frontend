7a746620a4783d25a57f4aafe036cd34ed1a17e2
extedcouD <89811591+extedcouD@users.noreply.github.com>
2024-12-20 00:49:36 +0530

    changes

--- backend/src/config/yamlConfig.ts
+++ backend/src/config/yamlConfig.ts
@@ -1,44 +1,47 @@
-import * as fs from 'fs';
-import * as yaml from 'js-yaml';
-import * as path from 'path';
+import * as fs from "fs";
+import * as yaml from "js-yaml";
+import * as path from "path";
 
-const configPath = "../yamlConfig/index.yaml"
+const configPath = "../yamlConfig/index.yaml";
 
 interface Config {
-    domain: Array<{
-        name: string;
-        flows: Array<{
-            id: string;
-            description: string;
-            sequence: {
-                [key: string]: {
-                    description: string;
-                };
-            };
-        }>;
-    }>;
+	domain: Array<{
+		name: string;
+		flows: {
+			id: string;
+			description: string;
+			sequence: {
+				[key: string]: {
+					key: string;
+					type: string;
+					pair: string | null;
+					unsolicited: boolean;
+					description: string;
+				};
+			}[];
+		}[];
+	}>;
 }
 
 const loadConfig = (filePath: string): Config => {
-    try {
-        const configPath = path.resolve(__dirname, filePath);
-        const fileContents = fs.readFileSync(configPath, 'utf8');
-        const config = yaml.load(fileContents) as Config;
-        return config;
-    } catch (e: any) {
-        console.error(`Failed to load config file: ${e.message}`);
-        throw e;
-    }
+	try {
+		const configPath = path.resolve(__dirname, filePath);
+		const fileContents = fs.readFileSync(configPath, "utf8");
+		const config = yaml.load(fileContents) as Config;
+		return config;
+	} catch (e: any) {
+		console.error(`Failed to load config file: ${e.message}`);
+		throw e;
+	}
 };
 
 let cachedConfig: Config | null = null;
 
 const getConfig = (): Config => {
-    if (!cachedConfig) {
-        cachedConfig = loadConfig(configPath);
-    }
-    return cachedConfig;
+	if (!cachedConfig) {
+		cachedConfig = loadConfig(configPath);
+	}
+	return cachedConfig;
 };
-
 
 export default getConfig;
--- backend/src/interfaces/sessionData.ts
+++ backend/src/interfaces/sessionData.ts
@@ -1,51 +1,48 @@
 // src/interfaces/sessionData.ts
 
-
-type sessionId = string
-type subscriberUrl = string
-type participantType = "BPP"|"BAP"
+type sessionId = string;
+type subscriberUrl = string;
+type participantType = "BPP" | "BAP";
 
 export interface SessionData {
-  city: string;
-  createdAt: string; // ISO timestamp
-  domain: string;
-  flowId?: string;
-  participantType: participantType;
-  sessionId: sessionId;
-  subscriberId: string;
-  subscriberUrl: subscriberUrl;
-  transactions: Record<string, TransactionDetails>; // Example transactions object
-  version: string;
-}
-  
-export interface TransactionDetails {
-  createdAt: string; // ISO timestamp
-  data: Record<string, any>; // Additional transaction details
-  state: string; // State of the transaction
-  transactionMode: string; // Type of transaction (auto/manual)
+	city: string;
+	createdAt: string; // ISO timestamp
+	domain: string;
+	flowId?: string;
+	participantType: participantType;
+	sessionId: sessionId;
+	subscriberId: string;
+	subscriberUrl: subscriberUrl;
+	transactions: Record<string, TransactionDetails>; // Example transactions object
+	version: string;
 }
 
+export interface TransactionDetails {
+	createdAt: string; // ISO timestamp
+	data: Record<string, any>; // Additional transaction details
+	state: string; // State of the transaction
+	transactionMode: string; // Type of transaction (auto/manual)
+}
 
 export interface TransformedSessionData {
-  active_session_id: sessionId;
-  type: participantType;
-  domain: string;
-  version: string;
-  city: string;
-  np_id: string;
-  current_flow_id?: string;
-  session_payloads: Record<string, any>;
-  context_cache: ContextCache;
+	active_session_id: sessionId;
+	type: participantType;
+	domain: string;
+	version: string;
+	city: string;
+	np_id: string;
+	current_flow_id?: string;
+	session_payloads: Record<string, any[]>;
+	context_cache: ContextCache;
+	difficulty_cache: Record<string, boolean>;
 }
 
 export interface ContextCache {
-  latest_timestamp: string; // ISO timestamp
-  latest_action: string;
-  subscriber_id: string;
-  subscriber_url: subscriberUrl;
-  message_ids: string[];
+	latest_timestamp: string; // ISO timestamp
+	latest_action: string;
+	subscriber_id: string;
+	subscriber_url: subscriberUrl;
+	message_ids: string[];
 }
 
-
-
-export type SessionKeyType = sessionId | subscriberUrl
+export type SessionKeyType = sessionId | subscriberUrl;
