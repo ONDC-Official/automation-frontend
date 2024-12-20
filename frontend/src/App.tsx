5782d0a98b0e53e594572ebfeb39b7a75aebd75a
extedcouD <extedcoud@gmail.com>
2024-12-20 16:00:44 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/index";
-import Transaction from "./pages/Transaction/index";
-import Flows from "./pages/Flows";
-import VerifyPayload from "./pages/VerifyPayload";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import MainContent from "./components/main-content";
+import TopBar from "./components/top-bar";
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/flows" element={<Flows />} />
-      <Route path="/transaction" element={<Transaction />} />
-      <Route path="/verify" element={<VerifyPayload />} />
-    </Routes>
-  );
-};
+function App() {
+	return (
+		<>
+			<TopBar />
+			<main className=" pt-16 h-full flex">
+				<MainContent />
+			</main>
+			<ToastContainer
+				position="bottom-right"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss={false}
+				draggable
+				pauseOnHover={false}
+				theme="colored"
+			/>
+		</>
+	);
+}
 
 export default App;
