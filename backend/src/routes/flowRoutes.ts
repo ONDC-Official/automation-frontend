50f1afa28cc40efc2c973d96a75def8e332fdbdb
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2025-01-08 11:53:48 +0530

    added config

--- backend/src/routes/flowRoutes.ts
+++ backend/src/routes/flowRoutes.ts
@@ -1,11 +1,20 @@
-import { Router } from 'express';
-import { fetchConfig, generateReport, handleTriggerRequest, validatePayload } from '../controllers/flowController';
+import { Router } from "express";
+import {
+  fetchConfig,
+  generateReport,
+  handleTriggerRequest,
+  validatePayload,
+  getPredefinedFlows,
+  getExample,
+} from "../controllers/flowController";
 
 const router = Router();
 
-router.get('/', fetchConfig);
-router.get('/report', generateReport);
-router.post('/trigger', handleTriggerRequest);
-router.post("/validate/:action", validatePayload)
+router.get("/", fetchConfig);
+router.get("/report", generateReport);
+router.post("/trigger", handleTriggerRequest);
+router.post("/validate/:action", validatePayload);
+router.get("/customFlow", getPredefinedFlows);
+router.post("/examples", getExample);
 
 export default router;
