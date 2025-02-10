b9075b3182c0364ba2b2c4cf25011a0c74ba786e
extedcouD <extedcoud@gmail.com>
2025-02-10 15:58:18 +0530

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import routes from './routes/index';
-import { redisService } from "ondc-automation-cache-lib";
-import cookieParser from 'cookie-parser';
+import express from "express";
+import routes from "./routes/index";
+import { RedisService } from "ondc-automation-cache-lib";
+import cookieParser from "cookie-parser";
 // import redisClient from './config/redisConfig'; // Import the Redis client
-import session from 'express-session';
-import redisClient from './config/redisConfig';
-import cors from "cors"
+import session from "express-session";
+import redisClient from "./config/redisConfig";
+import cors from "cors";
 const RedisStore = require("connect-redis").default;
 
 const app = express();
 
 // Select and use database 0
-redisService.useDb(0);
+RedisService.useDb(0);
 
 // Log Redis connection status
 // redisClient.on('connect', () => {
@@ -22,23 +22,24 @@ redisService.useDb(0);
 //     console.error('Redis connection error:', err);
 // });
 
-
 let redisStore = new RedisStore({
-    client: redisService,
+	client: RedisService,
 });
 
-  app.use(session({
-    store: redisStore,
-    secret: process.env.SESSION_SECRET || 'your-secret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.NODE_ENV === 'production', // For production, set to true
-      maxAge: 24 * 60 * 60 * 1000 // 1 day expiration
-    }
-  }));
+app.use(
+	session({
+		store: redisStore,
+		secret: process.env.SESSION_SECRET || "your-secret",
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			secure: process.env.NODE_ENV === "production", // For production, set to true
+			maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
+		},
+	})
+);
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 // Middleware to parse cookies
 app.use(cookieParser());
