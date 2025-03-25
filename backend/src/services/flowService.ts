5066fd00b51faf9fd14ea2cacc61e9e4bfca9861
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2025-03-25 17:52:43 +0530

    udpated mock base

--- backend/src/services/flowService.ts
+++ backend/src/services/flowService.ts
@@ -11,21 +11,3 @@ export const fetchConfigService = () => {
 	}
 	return config;
 };
-
-export const triggerMockService = async (
-	triggerInput: TriggerInput
-): Promise<string> => {
-	try {
-		const response = await axios.post(
-			`${process.env.MOCK_SERVICE as string}/trigger`
-		);
-	} catch (error: any) {
-		if (error.response && error.response.status === 400) {
-			return "fail";
-		} else if (error.response && error.response.status === 500) {
-			return "error";
-		}
-		throw error;
-	}
-	return "unknown";
-};
