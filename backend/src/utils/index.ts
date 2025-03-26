73f2d3005535de6596c62cc747fa601e0b3bb295
extedcouD <extedcoud@gmail.com>
2025-03-26 10:58:50 +0530

    changes

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -2,12 +2,12 @@ import { getSessionService } from "../services/sessionService";
 import logger from "./logger";
 
 export const buildMockBaseURL = async (url: string, sessionId: string) => {
-  const sessionData = await getSessionService(sessionId);
+	const sessionData = await getSessionService(sessionId);
 
-  const generatedURL = `${process.env.MOCK_SERVICE as string}/${
-    sessionData.domain
-  }/${sessionData.version}/${url}`;
+	const generatedURL = `${process.env.MOCK_SERVICE as string}/${
+		sessionData.domain
+	}/${sessionData.version}/${url}`;
 
-  logger.info("generated mock urk: ", generatedURL);
-  return generatedURL;
+	logger.info("generated mock urk: " + generatedURL);
+	return generatedURL;
 };
