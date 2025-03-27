e0d2977218ae30bf9b56356e2bfaad44df252b66
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2025-03-27 14:53:42 +0530

    guide changes

--- frontend/src/components/flow-testing/guides.tsx
+++ frontend/src/components/flow-testing/guides.tsx
@@ -2,7 +2,6 @@ export function FormGuide() {
   return (
     <div className="w-full bg-gray-50 border border-gray-200 p-6">
       <h3 className="text-xl font-semibold text-gray-800 mb-4">Guide</h3>
-
       <p className="text-gray-600 mb-4">
         The Scenario Testing Tool enabling testing of complete ONDC workflows
         across buyer and seller interactions across various use cases and
@@ -12,19 +11,53 @@ export function FormGuide() {
         initiate the flow testing process. Process & Send payloads are per the
         flow and issues, if any, are highlighted as you proceed step by step.
       </p>
-      <p className="text-gray-600">To test your app, send requests to:</p>
+      <p className="text-gray-600 mb-4">
+        To test your app, use the appropriate endpoint based on your role:
+      </p>
       <ul className="list-disc pl-6 mt-2 text-gray-600 space-y-2">
         <li>
+          <p className="text-gray-600 mb-4">
+            <b>If you are a Seller NP:</b> The protocol workbench will act as a
+            buyer network participant. Use this URL:
+          </p>
           <code className="bg-gray-100 text-gray-800 px-2 py-1 rounded">
-            {import.meta.env.VITE_BAP_URL}
-          </code>{" "}
-          for Buyer testing.
+            {`${import.meta.env.VITE_BASE_URL}/<domain>/<version>/buyer`}
+          </code>
         </li>
         <li>
+          <p className="text-gray-600 mb-4">
+            <b>If you are a Buyer NP:</b> The protocol workbench will act as a
+            seller network participant. Use this URL:
+          </p>
           <code className="bg-gray-100 text-gray-800 px-2 py-1 rounded">
-            {import.meta.env.VITE_BPP_URL}
+            {`${import.meta.env.VITE_BASE_URL}/<domain>/<version>/seller`}
           </code>{" "}
-          for Seller testing.
+        </li>
+      </ul>
+      <p className="text-gray-600 mb-6 mt-6">
+        <b>Example</b>
+      </p>
+      <p className="text-gray-600 mb-4">
+        If the domain is <b>ONDC:TRV11</b> and the version is <b>2.0.0</b>, the
+        request URLs will be:
+      </p>
+      <ul className="list-disc pl-6 mt-2 text-gray-600 space-y-2">
+        <li>
+          <p className="text-gray-600 mb-4">
+            Protocol workbench endpoint (acting as a buyer network participant):
+          </p>
+          <code className="bg-gray-100 text-gray-800 px-2 py-1 rounded">
+            {`${import.meta.env.VITE_BASE_URL}/ONDC:TRV11/2.0.0/buyer`}
+          </code>
+        </li>
+        <li>
+          <p className="text-gray-600 mb-4">
+            Protocol workbench endpoint (acting as a seller network
+            participant):
+          </p>
+          <code className="bg-gray-100 text-gray-800 px-2 py-1 rounded">
+            {`${import.meta.env.VITE_BASE_URL}/ONDC:TRV11/2.0.0/seller`}
+          </code>{" "}
         </li>
       </ul>
     </div>
