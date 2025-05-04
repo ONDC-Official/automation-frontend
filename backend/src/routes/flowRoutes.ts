57c0b991e28d7d803a86420a18654917e1be304c
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2025-05-05 00:47:04 +0530

    feat: tracing added

--- backend/src/routes/flowRoutes.ts
+++ backend/src/routes/flowRoutes.ts
@@ -11,20 +11,38 @@ import {
 	newFlow,
 } from "../controllers/flowController";
 import validateRequiredParams from "../middlewares/generic";
+import otelTracing from "../services/tracing-service";
 
 const router = Router();
 
 router.get("/", fetchConfig);
-router.post("/report", generateReport);
-router.post("/trigger/:action", handleTriggerRequest);
-router.post("/validate/:action", validatePayload);
+router.post("/report", 
+	otelTracing('', 'query.sessionId'),
+	generateReport
+);
+router.post("/trigger/:action", 
+	otelTracing('query.transaction_id', 'query.session_id', 'query.subscriber_url'),
+	handleTriggerRequest
+);
+router.post("/validate/:action", 
+	otelTracing('body.context.transaction_id', '', 'body.context.bap_id', 'body.context.bpp_id'),
+	validatePayload
+);
 router.get("/customFlow", getPredefinedFlows);
 router.post("/examples", getExample);
 router.get(
 	"/current-state",
 	validateRequiredParams(["session_id", "transaction_id"]),
+	otelTracing('query.transaction_id', 'query.session_id'),
 	getCurrentStateFlow
 );
-router.post("/proceed", proceedFlow);
-router.post("/new", newFlow);
+router.post("/proceed", 
+	otelTracing('body.transaction_id', 'body.session_id'),
+	proceedFlow
+);
+router.post("/new", 
+	otelTracing('body.transaction_id', 'body.session_id'),
+	newFlow
+);
+
 export default router;
--- backend/src/routes/sessionRoutes.ts
+++ backend/src/routes/sessionRoutes.ts
@@ -10,11 +10,12 @@ import {
 	updateSession,
 } from "../controllers/sessionController";
 import validateRequiredParams from "../middlewares/generic";
+import otelTracing from "../services/tracing-service";
 
 const router = Router();
 
 // Define routes and use the correct async handler types
-router.post("/", createSession);
+router.post("/", otelTracing('body.transaction_id', 'body.session_id', 'body.bap_id', 'body.bpp_id'), createSession);
 
 router.post(
 	"/expectation",
@@ -24,34 +25,39 @@ router.post(
 		"expected_action",
 		"session_id",
 	]),
+	otelTracing('', 'query.session_id'),
 	createExpectation
 );
 
-router.get("/", getSession);
+router.get("/", otelTracing('', 'query.session_id'), getSession);
 
 router.get(
 	"/transaction",
 	validateRequiredParams(["transaction_id", "subscriber_url"]),
+	otelTracing('query.transaction_id'),
 	getTransactionData
 );
 
 router.get(
 	"/flowPermission",
 	validateRequiredParams(["subscriber_url", "action"]),
+	otelTracing('', '', 'query.subscriber_url'),
 	requestForFlowPermission
 );
 
-router.put("/", updateSession);
+router.put("/", otelTracing('body.transaction_id', 'body.session_id', 'body.bap_id', 'body.bpp_id'), updateSession);
 
 router.delete(
 	"/clearFlow",
 	validateRequiredParams(["session_id", "flow_id"]),
+	otelTracing('', 'query.session_id'),
 	clearFlow
 );
 
 router.delete(
 	"/expectation",
 	validateRequiredParams(["subscriber_url", "session_id"]),
+	otelTracing('', 'query.session_id'),
 	deleteExpectation
 );
 
