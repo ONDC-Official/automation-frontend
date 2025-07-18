befc5454a366174d2f6a516fc626785579001f76
extedcouD <extedcoud@gmail.com>
2025-07-18 16:00:27 +0530

    chnages

---
+++ backend/src/services/registryServices.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const verifyJWT = async (token: string) => {
+	try {
+		const registryBaseUrl = process.env.REGISTRY_SERVICE;
+		const url = `${registryBaseUrl}/admin/v3.0/token/verify`;
+
+		const res = await axios.post(url, {
+			token: token,
+		});
+		if (res.data.Valid) {
+			return {
+				githubId: res.data.Claims.github_id,
+				participantId: res.data.Claims.participant_id,
+			};
+		}
+		return undefined;
+	} catch (error) {
+		console.error("JWT verification failed:", error);
+		return undefined;
+	}
+};
