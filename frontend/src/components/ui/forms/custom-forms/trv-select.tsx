93deac9dc197b0b81ef8dd181e28487414ab3cf0
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2025-08-22 16:42:03 +0530

    removed preview button

--- frontend/src/components/ui/forms/custom-forms/trv-select.tsx
+++ frontend/src/components/ui/forms/custom-forms/trv-select.tsx
@@ -238,7 +238,7 @@ export default function TRVSelect({
         >
           Submit
         </button>
-        <button
+        {/* <button
           type="button"
           onClick={() => {
             const values = getValues();
@@ -247,7 +247,7 @@ export default function TRVSelect({
           className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700"
         >
           Preview Data
-        </button>
+        </button> */}
       </form>
     </div>
   );
