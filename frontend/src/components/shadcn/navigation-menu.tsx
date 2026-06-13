a2cf4f699c462d1dea9910d263a71c5dc3d1db60
ashishjainondc <ashish.jain@ondc.org>
2026-06-13 09:46:55 +0530

    feat: revamp initial commit

--- frontend/src/components/shadcn/navigation-menu.tsx
+++ frontend/src/components/shadcn/navigation-menu.tsx
@@ -89,13 +89,13 @@ function NavigationMenuTrigger({
 
 function NavigationMenuContent({
     className,
-    forceMount,
+    forceMount = true,
     ...props
 }: ComponentProps<typeof NavigationMenuPrimitive.Content>) {
     return (
         <NavigationMenuPrimitive.Content
             data-slot="navigation-menu-content"
-            forceMount
+            forceMount={forceMount}
             className={cn(
                 "top-0 left-0 w-full p-2 pr-2.5 data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 data-[motion^=from-]:animate-in data-[motion^=from-]:fade-in data-[motion^=to-]:animate-out data-[motion^=to-]:fade-out md:absolute md:w-auto",
                 "group-data-[viewport=false]/navigation-menu:top-full group-data-[viewport=false]/navigation-menu:mt-1.5 group-data-[viewport=false]/navigation-menu:z-50 group-data-[viewport=false]/navigation-menu:w-max group-data-[viewport=false]/navigation-menu:min-w-max group-data-[viewport=false]/navigation-menu:overflow-hidden group-data-[viewport=false]/navigation-menu:rounded-md group-data-[viewport=false]/navigation-menu:border group-data-[viewport=false]/navigation-menu:bg-popover group-data-[viewport=false]/navigation-menu:text-popover-foreground group-data-[viewport=false]/navigation-menu:shadow group-data-[viewport=false]/navigation-menu:duration-200 group-data-[viewport=false]/navigation-menu:**:data-[slot=navigation-menu-link]:whitespace-nowrap **:data-[slot=navigation-menu-link]:focus:ring-0 **:data-[slot=navigation-menu-link]:focus:outline-none data-[state=closed]:pointer-events-none data-[state=closed]:invisible data-[state=closed]:absolute data-[state=closed]:h-0 data-[state=closed]:overflow-hidden data-[state=closed]:border-0 data-[state=closed]:p-0 data-[state=closed]:opacity-0 data-[state=open]:visible data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95",
